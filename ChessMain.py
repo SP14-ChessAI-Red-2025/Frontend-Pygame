395f0784944f2061f344310ce6dc147611e55afc
Alec Walsh <code@alecwalsh.name>
2025-04-27 13:57:34 -0400

    Move rendering to its own file

--- ChessMain.py
+++ ChessMain.py
@@ -3,36 +3,11 @@ from sys import exit, argv
 from pygame import Color
 
 import chess
+import render
 
-# defines screen size, board size, square size, and piece icon array
-BOARD_SIZE = 8
-SQ_SIZE = 0
+
 SELECTED_PIECE = ("", 0, 0)
-icons = []
 
-
-#loads images
-def load_images():
-    pcs = ["wht_pawn", "wht_knight", "wht_bishop", "wht_rook", "wht_queen", "wht_king",
-           "blk_pawn", "blk_knight", "blk_bishop", "blk_rook", "blk_queen", "blk_king"]
-    for pc in pcs:
-        icons.append(pygame.transform.scale(pygame.image.load("images/" + pc + ".png"), (SQ_SIZE, SQ_SIZE)))
-
-#draws game board pattern
-def draw_board(screen):
-    colors = [pygame.Color("antiquewhite"), pygame.Color("lightseagreen")]
-    for row in range(BOARD_SIZE):
-        for column in range(BOARD_SIZE):
-            color = colors[(row + column) % 2]
-            pygame.draw.rect(screen, color, pygame.Rect(column * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
-
-def icon_for_piece(piece_type, piece_player):
-    assert piece_type != 0
-
-    if piece_player == 0:
-        return icons[piece_type - 1]
-    else:
-        return icons[6 + piece_type - 1]
 
 # Given a chess move, return the square that must be clicked to apply the move
 # For most moves, this is just move.target_position, but castling needs special handling
@@ -46,25 +21,6 @@ def target_for_move(move):
             return (move.start_position.rank, 6)
 
     return (move.target_position.rank, move.target_position.file)
-
-#draws pieces over game board
-def draw_pcs(screen, game_board, valid_moves):
-    for row in range(BOARD_SIZE):
-        for column in range(BOARD_SIZE):
-            pc = game_board[7 - row][column]
-            #
-            pcCoords = (pc, row, column)
-
-            if (7 - row, column) in [target_for_move(move) for move in valid_moves]:
-                pygame.draw.circle(screen, pygame.Color("black"), center = (column * SQ_SIZE + SQ_SIZE / 2, row * SQ_SIZE + SQ_SIZE / 2), radius = SQ_SIZE / 2, width = 2)
-
-            if pc.piece_type != 0:
-                icon = icon_for_piece(pc.piece_type, pc.piece_player)
-
-                if pcCoords != SELECTED_PIECE:
-                    screen.blit(icon, pygame.Rect(SQ_SIZE * column, SQ_SIZE * row, SQ_SIZE, SQ_SIZE))
-                else:
-                    screen.blit(icon, pygame.Rect(pygame.mouse.get_pos()[0] - (SQ_SIZE / 2), pygame.mouse.get_pos()[1] - (SQ_SIZE / 2), SQ_SIZE, SQ_SIZE))
 
 def moves_for_position(valid_moves, rank, file):
     # Don't include moves of type castle, claim_draw, or resign, as they interpret move.target_position differently
@@ -94,22 +50,7 @@ def main():
     # initializes pygame
     pygame.init()
 
-    #calculates window size and square size
-    monitorInfo = pygame.display.Info()
-    screenWidth = screenHeight = monitorInfo.current_h / 1.25
-    global SQ_SIZE
-    SQ_SIZE = screenWidth / BOARD_SIZE
-
-    #sets window size
-    screen = pygame.display.set_mode((screenWidth, screenHeight))
-
-    # sets window caption and icon
-    pygame.display.set_caption("ChessAI")
-    window_icon = pygame.image.load("images/icon.png")
-    pygame.display.set_icon(window_icon)
-
-    #loads images
-    load_images()
+    renderer = render.Renderer()
 
     ai_turn = True
 
@@ -133,11 +74,11 @@ def main():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
-                #
+                
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     position = pygame.mouse.get_pos()
-                    column = (int)(position[0] / SQ_SIZE)
-                    row = (int)(position [1] / SQ_SIZE)
+                    column = (int)(position[0] / renderer.SQ_SIZE)
+                    row = (int)(position [1] / renderer.SQ_SIZE)
 
                     global SELECTED_PIECE
 
@@ -173,9 +114,8 @@ def main():
                     if made_move:
                         ai_turn = ai_enabled
 
-            #draw board & pieces
-            draw_board(screen)
-            draw_pcs(screen, board_state.pieces, valid_moves)
+            move_targets = [target_for_move(move) for move in valid_moves]
+            renderer.render(board_state, move_targets, SELECTED_PIECE)
 
             pygame.display.update()
 
---
+++ render.py
@@ -0,0 +1,74 @@
+import pygame
+from pygame import Color
+
+class Renderer:
+    def __init__(self):
+        # defines screen size, board size, square size, and piece icon array
+        self.BOARD_SIZE = 8
+        self.icons = []
+
+        #calculates window size and square size
+        monitorInfo = pygame.display.Info()
+        screenWidth = screenHeight = monitorInfo.current_h / 1.25
+        
+        self.SQ_SIZE = screenWidth / self.BOARD_SIZE
+
+        #sets window size
+        self.screen = pygame.display.set_mode((screenWidth, screenHeight))
+
+        # sets window caption and icon
+        pygame.display.set_caption("ChessAI")
+        window_icon = pygame.image.load("images/icon.png")
+        pygame.display.set_icon(window_icon)
+
+        #loads images
+        self.load_images()
+
+    #loads images
+    def load_images(self):
+        pcs = ["wht_pawn", "wht_knight", "wht_bishop", "wht_rook", "wht_queen", "wht_king",
+            "blk_pawn", "blk_knight", "blk_bishop", "blk_rook", "blk_queen", "blk_king"]
+        for pc in pcs:
+            self.icons.append(pygame.transform.scale(pygame.image.load("images/" + pc + ".png"), (self.SQ_SIZE, self.SQ_SIZE)))
+
+    #draws game board pattern
+    def draw_board(self):
+        colors = [pygame.Color("antiquewhite"), pygame.Color("lightseagreen")]
+        for row in range(self.BOARD_SIZE):
+            for column in range(self.BOARD_SIZE):
+                color = colors[(row + column) % 2]
+                pygame.draw.rect(self.screen, color, pygame.Rect(column * self.SQ_SIZE, row * self.SQ_SIZE, self.SQ_SIZE, self.SQ_SIZE))
+
+    def icon_for_piece(self, piece_type, piece_player):
+        assert piece_type != 0
+
+        if piece_player == 0:
+            return self.icons[piece_type - 1]
+        else:
+            return self.icons[6 + piece_type - 1]
+
+    #draws pieces over game board
+    def draw_pcs(self, game_board, move_targets, SELECTED_PIECE):
+        SQ_SIZE = self.SQ_SIZE
+        
+        for row in range(self.BOARD_SIZE):
+            for column in range(self.BOARD_SIZE):
+                pc = game_board[7 - row][column]
+                
+                pcCoords = (pc, row, column)
+
+                if (7 - row, column) in move_targets:
+                    pygame.draw.circle(self.screen, pygame.Color("black"), center = (column * SQ_SIZE + SQ_SIZE / 2, row * SQ_SIZE + SQ_SIZE / 2), radius = SQ_SIZE / 2, width = 2)
+
+                if pc.piece_type != 0:
+                    icon = self.icon_for_piece(pc.piece_type, pc.piece_player)
+
+                    if pcCoords != SELECTED_PIECE:
+                        self.screen.blit(icon, pygame.Rect(SQ_SIZE * column, SQ_SIZE * row, SQ_SIZE, SQ_SIZE))
+                    else:
+                        self.screen.blit(icon, pygame.Rect(pygame.mouse.get_pos()[0] - (SQ_SIZE / 2), pygame.mouse.get_pos()[1] - (SQ_SIZE / 2), SQ_SIZE, SQ_SIZE))
+
+    def render(self, board_state, move_targets, SELECTED_PIECE):
+        #draw board & pieces
+        self.draw_board()
+        self.draw_pcs(board_state.pieces, move_targets, SELECTED_PIECE)
